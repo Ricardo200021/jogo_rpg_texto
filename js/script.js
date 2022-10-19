621fdaa49c54d7000f002d7ab369484d18d61bb8
Ricardo C. Figueiró <86254137+Ricardo200021@users.noreply.github.com>
2022-10-19 00:31:56 -0300

    RPGtxt

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+//função com nome de playAudio
+//variavel let chamada x
+//essa variavel x recebe o elemento id chamada motor que está na tag audio do html
+//a função play() ativa o que estiver na variavel x
+
+//Script ligado a pagina 2 e 3;
+function playAudio() {
+    let x = document.getElementById("motor");
+    x.play();
+}
+
+//================================================================================================================
+
